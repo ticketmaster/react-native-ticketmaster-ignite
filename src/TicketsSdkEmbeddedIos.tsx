5291f78018d7e0113a2ae655b412f9e65b7220c1
Kotauror <zygmunt.justyna@gmail.com>
2024-07-25 15:42:34 +0200

    chore: add test for TicketsSdkEmbeddedIos

--- src/TicketsSdkEmbeddedIos.tsx
+++ src/TicketsSdkEmbeddedIos.tsx
@@ -27,5 +27,5 @@ export const TicketsSdkEmbeddedIos = ({
     setTimeout(() => setInitialFocus(false), renderTimeDelay || 0);
   }, [renderTimeDelay]);
 
-  return <>{!initialFocus && <TicketsSdk style={style || { flex: 1 }} />}</>;
+  return <>{!initialFocus && <TicketsSdk testID="TicketsSdk" style={style || { flex: 1 }} />}</>;
 };
