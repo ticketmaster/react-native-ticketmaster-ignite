3f3bebd5c4287af534c3b3f8dc25d3e6e25c5e14
Kotauror <zygmunt.justyna@gmail.com>
2024-08-30 13:32:56 +0200

    perf: remove unnecessary config from ios/TicketsEmbedded

--- ios/TicketsSdkEmbeddedViewController.swift
+++ ios/TicketsSdkEmbeddedViewController.swift
@@ -4,62 +4,20 @@ import TicketmasterTickets
 public class TicketsSdkEmbeddedViewController: UIViewController, TMTicketsAnalyticsDelegate {
     var ticketsView: TMTicketsView!
     
-    
     public override func viewDidLoad() {
         super.viewDidLoad()
         
         TMTickets.shared.analyticsDelegate = self
-        
-        let apiKey = Config.shared.get(for: "apiKey")
-        let tmxServiceSettings = TMAuthentication.TMXSettings(apiKey: apiKey,
-                                                              region: .US)
-        
-        let primaryColor = Config.shared.get(for: "primaryColor")
-        let backgroundColor = UIColor(hexString: primaryColor) ?? AppConstants.defaultBrandColor
-        
-        let branding = TMAuthentication.Branding(displayName: Config.shared.get(for: "clientName"), backgroundColor: backgroundColor, theme: .light)
-        
-        let brandedServiceSettings = TMAuthentication.BrandedServiceSettings(tmxSettings: tmxServiceSettings,
-                                                                             branding: branding)
-        
-        // configure TMAuthentication with Settings and Branding
-        print("Authentication SDK Configuring...")
-        TMAuthentication.shared.configure(brandedServiceSettings: brandedServiceSettings) {
-            backendsConfigured in
-            
-            // your API key may contain configurations for multiple backend services
-            // the details are not needed for most common use-cases
-            print(" - Authentication SDK Configured: \(backendsConfigured.count)")
-            
-            
-            // TMTickets inherits it's configuration and branding from TMAuthentication
-            print("Tickets SDK Configuring...")
-            TMTickets.shared.configure {
-                
-                // Tickets is configured, now we are ready to present TMTicketsViewController or TMTicketsView
-                print(" - Tickets SDK Configured")
-                
-                self.ticketsView = TMTicketsView.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: self.view.frame.height))
-                self.view.addSubview(self.ticketsView)
-                TMTickets.shared.start(ticketsView: self.ticketsView)
-                
-                
-                
-//                DispatchQueue.main.asyncAfter(deadline: .now() + 120) {
-//                    TMTickets.shared.analyticsDelegate = self
-//                }
-                
-                
-            } failure: { error in
-                // something went wrong, probably TMAuthentication was not configured correctly
-                print(" - Tickets SDK Configuration Error: \(error.localizedDescription)")
-            }
-            
+        print("Tickets SDK Configuring...")
+
+        TMTickets.shared.configure {
+            print(" - Tickets SDK Configured")
+            self.ticketsView = TMTicketsView.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: self.view.frame.height))
+            self.view.addSubview(self.ticketsView)
+            TMTickets.shared.start(ticketsView: self.ticketsView)
         } failure: { error in
-            // something went wrong, probably the wrong apiKey+region combination
-            print(" - Authentication SDK Configuration Error: \(error.localizedDescription)")
+            print(" - Tickets SDK Configuration Error: \(error.localizedDescription)")
         }
-        
     }
     
     public override func viewDidAppear(_ animated: Bool) {
