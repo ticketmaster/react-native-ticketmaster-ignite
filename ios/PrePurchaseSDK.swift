1e795890cf13f4f45f3444371a5e7ab661745b95
Daniel Olugbade <110384307+DanielOS7-TM@users.noreply.github.com>
2024-09-19 14:49:26 +0100

    fix: incorrect header on prepurchase edp flow (#35)
    * fix: incorrect header on prepurchase edp flow
    
    * chore: format file

--- ios/PrePurchaseSDK.swift
+++ ios/PrePurchaseSDK.swift
@@ -5,79 +5,82 @@ import TicketmasterFoundation
 
 @objc(PrePurchaseSDK)
 class PrePurchaseSDK: UIViewController, TMPrePurchaseNavigationDelegate {
-    var venueId: String = ""
-    var attractionId: String = ""
-    
-    var firstRender: Bool = true
-    
-    func setVenueId(_ venueId: String) {
-        self.venueId = venueId
+  var venueId: String = ""
+  var attractionId: String = ""
+  
+  var firstRender: Bool = true
+  
+  func setVenueId(_ venueId: String) {
+    self.venueId = venueId
+  }
+  
+  func setAttractionId(_ attractionId: String) {
+    self.attractionId = attractionId
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    if (firstRender) {
+      firstRender = false
+    } else {
+      self.dismiss(animated: true)
     }
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    func setAttractionId(_ attractionId: String) {
-        self.attractionId = attractionId
-    }
+    let apiKey = Config.shared.get(for: "apiKey")
+    let primaryColor = Config.shared.get(for: "primaryColor")
+    let backgroundColor = UIColor(hexString: primaryColor) ?? AppConstants.defaultBrandColor
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        if (firstRender) {
-            firstRender = false
-        } else {
-            self.dismiss(animated: true)
+    TMPrePurchase.shared.configure(apiKey: apiKey, completion: { isPrePurchaseApiSet in
+      print("PrePurchase api key set result: \(isPrePurchaseApiSet)")
+      
+      TMPrePurchase.shared.brandColor = backgroundColor!
+      
+      var viewController: TMPrePurchaseViewController
+      
+      if (self.venueId != "") {
+        print("Set viewController to Venue")
+        viewController = TMPrePurchaseViewController.venueDetailsViewController(venueIdentifier: self.venueId, enclosingEnvironment: .modalPresentation)
+      } else {
+        print("Set viewController to Attraction")
+        viewController = TMPrePurchaseViewController.attractionDetailsViewController(attractionIdentifier: self.attractionId, enclosingEnvironment: .modalPresentation)
+      }
+      
+      viewController.modalPresentationStyle = .fullScreen
+      viewController.navigationDelegate = self
+      self.present(viewController, animated: false)
+    })
+  }
+  
+  func sendEvent(_ name: String, body: [String : Any]) {
+    EventEmitter.emitter.sendEvent(withName: name, body: body)
+  }
+  
+  //    func prePurchaseViewController(_ viewController: TicketmasterPrePurchase.TMPrePurchaseViewController, didShare pageTitle: String, and pageURL: URL, to activityType: UIActivity.ActivityType) {
+  //        sendEvent("igniteAnalytics", body: ["prePurchaseSdkDidShare": "\(pageTitle)"])
+  //    }
+  //
+  func prePurchaseViewController(_ viewController: TicketmasterPrePurchase.TMPrePurchaseViewController, didFirePageView pageView: TicketmasterFoundation.UALPageView) {
+    //        sendEvent("igniteAnalytics", body: ["prePurchaseSdkDidFirePageView": "\(pageView)"])
+    return
+  }
+  
+  func prePurchaseViewController(_ viewController: TicketmasterPrePurchase.TMPrePurchaseViewController, navigateToEventDetailsPageWithIdentifier eventIdentifier: String) {
+    let configuration = TMPurchaseWebsiteConfiguration(eventID: eventIdentifier)
+    let apiKey = Config.shared.get(for: "apiKey")
+    let primaryColor = Config.shared.get(for: "primaryColor")
+    let backgroundColor = UIColor(hexString: primaryColor) ?? AppConstants.defaultBrandColor
+    TMPurchase.shared.configure(apiKey: apiKey, completion: {isPurchaseApiSet in
+      TMPurchase.shared.brandColor = backgroundColor!
+      TMDiscoveryAPI.shared.configure(apiKey: apiKey, completion: { isDiscoveryApiSet in
+        if (isDiscoveryApiSet && isPurchaseApiSet) {
+          let purchaseNavController = TMPurchaseNavigationController(configuration: configuration)
+          viewController.present(purchaseNavController, animated: true, completion: nil)
         }
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let apiKey = Config.shared.get(for: "apiKey")
-        let primaryColor = Config.shared.get(for: "primaryColor")
-        let backgroundColor = UIColor(hexString: primaryColor) ?? AppConstants.defaultBrandColor
-
-        TMPrePurchase.shared.brandColor = backgroundColor!
-        TMPrePurchase.shared.configure(apiKey: apiKey, completion: { isPrePurchaseApiSet in
-            print("PrePurchase api key set result: \(isPrePurchaseApiSet)")
-            var viewController: TMPrePurchaseViewController
-
-            if (self.venueId != "") {
-                print("Set viewController to Venue")
-                viewController = TMPrePurchaseViewController.venueDetailsViewController(venueIdentifier: self.venueId, enclosingEnvironment: .modalPresentation)
-            } else {
-                print("Set viewController to Attraction")
-                viewController = TMPrePurchaseViewController.attractionDetailsViewController(attractionIdentifier: self.attractionId, enclosingEnvironment: .modalPresentation)
-            }
-
-            viewController.modalPresentationStyle = .fullScreen
-            viewController.navigationDelegate = self
-            self.present(viewController, animated: false)
-        })
-    }
-    
-    
-    func sendEvent(_ name: String, body: [String : Any]) {
-        EventEmitter.emitter.sendEvent(withName: name, body: body)
-    }
-    
-//    func prePurchaseViewController(_ viewController: TicketmasterPrePurchase.TMPrePurchaseViewController, didShare pageTitle: String, and pageURL: URL, to activityType: UIActivity.ActivityType) {
-//        sendEvent("igniteAnalytics", body: ["prePurchaseSdkDidShare": "\(pageTitle)"])
-//    }
-//    
-    func prePurchaseViewController(_ viewController: TicketmasterPrePurchase.TMPrePurchaseViewController, didFirePageView pageView: TicketmasterFoundation.UALPageView) {
-//        sendEvent("igniteAnalytics", body: ["prePurchaseSdkDidFirePageView": "\(pageView)"])
-        return
-    }
-    
-    func prePurchaseViewController(_ viewController: TicketmasterPrePurchase.TMPrePurchaseViewController, navigateToEventDetailsPageWithIdentifier eventIdentifier: String) {
-        let configuration = TMPurchaseWebsiteConfiguration(eventID: eventIdentifier)
-        let apiKey = Config.shared.get(for: "apiKey")
-        TMPurchase.shared.configure(apiKey: apiKey, completion: {isPurchaseApiSet in
-            TMDiscoveryAPI.shared.configure(apiKey: apiKey, completion: { isDiscoveryApiSet in
-                if (isDiscoveryApiSet && isPurchaseApiSet) {
-                    let purchaseNavController = TMPurchaseNavigationController(configuration: configuration)
-                    viewController.present(purchaseNavController, animated: true, completion: nil)
-                }
-            })
-        })
-    }
+      })
+    })
+  }
 }
--- ios/PurchaseSDK.swift
+++ ios/PurchaseSDK.swift
@@ -4,101 +4,102 @@ import TicketmasterFoundation
 
 @objc(PurchaseSDK)
 class PurchaseSDK: UIViewController, TMPurchaseUserAnalyticsDelegate, TMPurchaseWebAnalyticsDelegate {
-    var eventId: String = ""
-    
-    var firstRender: Bool = true
-    
-    func setEventId(_ eventId: String) {
-        self.eventId = eventId
+  var eventId: String = ""
+  
+  var firstRender: Bool = true
+  
+  func setEventId(_ eventId: String) {
+    self.eventId = eventId
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    if (firstRender) {
+      firstRender = false
+    } else {
+      self.dismiss(animated: true)
     }
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        if (firstRender) {
-            firstRender = false
-        } else {
-            self.dismiss(animated: true)
-        }
-    }
+    let apiKey = Config.shared.get(for: "apiKey")
+    let primaryColor = Config.shared.get(for: "primaryColor")
+    let backgroundColor = UIColor(hexString: primaryColor) ?? AppConstants.defaultBrandColor
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let apiKey = Config.shared.get(for: "apiKey")
-        let primaryColor = Config.shared.get(for: "primaryColor")
-        let backgroundColor = UIColor(hexString: primaryColor) ?? AppConstants.defaultBrandColor
-
-        TMPurchase.shared.brandColor = backgroundColor!
-        TMPurchase.shared.configure(apiKey: apiKey, completion: { isPurchaseApiSet in
-            print("Purchase api key set result: \(isPurchaseApiSet)")
-
-            let edpNav = TMPurchaseNavigationController.eventDetailsNavigationController(eventIdentifier: self.eventId, marketDomain: .US)
-            edpNav.modalPresentationStyle = .fullScreen
-            edpNav.userAnalyticsDelegate =  self
-            edpNav.webAnalyticsDelegate =  self
-            self.present(edpNav, animated: false)
-        })
-    }
-    
-    func sendEvent(_ name: String, body: [String : Any]) {
-        EventEmitter.emitter.sendEvent(withName: name, body: body)
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, pageLoadDidErrorFor url: URL, error: NSError) {
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, webPageDidErrorFor url: URL, errorString: String) {
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, webPageDidReportUALPageView pageView: TicketmasterFoundation.UALPageView) {
-//        sendEvent("igniteAnalytics", body: ["purchaseSdkWebPageDidReportUALPageView:": "\(pageView)"])
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, webPageDidReportUALCommerceEvent commerceEvent: TicketmasterFoundation.UALCommerceEvent) {
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didBeginTicketSelectionFor event: TicketmasterDiscoveryAPI.DiscoveryEvent) {
-//        sendEvent("igniteAnalytics", body: ["purchaseSdkDidBeginTicketSelectionFor:": "\(event)"])
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didEndTicketSelectionFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, because reason: TicketmasterPurchase.TMEndTicketSelectionReason) {
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didBeginCheckoutFor event: TicketmasterDiscoveryAPI.DiscoveryEvent) {
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didEndCheckoutFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, because reason: TicketmasterPurchase.TMEndCheckoutReason) {
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didMakePurchaseFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, order: TicketmasterPurchase.TMPurchaseOrder) {
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didPressNavBarButtonFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, button: TicketmasterPurchase.TMPurchaseNavBarButton) {
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didShare event: TicketmasterDiscoveryAPI.DiscoveryEvent, activityType: UIActivity.ActivityType) {
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didViewSubPageFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, subPage: TicketmasterPurchase.TMPurchaseSubPage) {
-//        sendEvent("igniteAnalytics", body: ["purchaseSdkDidViewSubPageFor:": "\(event)"])
-        return
-    }
-    
-    func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didMakeDecisionFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, component: TicketmasterPurchase.TMPurchaseComponent, decision: TicketmasterPurchase.TMPurchaseDecision) {
-        return
-    }
+    TMPurchase.shared.configure(apiKey: apiKey, completion: { isPurchaseApiSet in
+      print("Purchase api key set result: \(isPurchaseApiSet)")
+      
+      TMPurchase.shared.brandColor = backgroundColor!
+      
+      let edpNav = TMPurchaseNavigationController.eventDetailsNavigationController(eventIdentifier: self.eventId, marketDomain: .US)
+      edpNav.modalPresentationStyle = .fullScreen
+      edpNav.userAnalyticsDelegate =  self
+      edpNav.webAnalyticsDelegate =  self
+      self.present(edpNav, animated: false)
+    })
+  }
+  
+  func sendEvent(_ name: String, body: [String : Any]) {
+    EventEmitter.emitter.sendEvent(withName: name, body: body)
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, pageLoadDidErrorFor url: URL, error: NSError) {
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, webPageDidErrorFor url: URL, errorString: String) {
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, webPageDidReportUALPageView pageView: TicketmasterFoundation.UALPageView) {
+    //        sendEvent("igniteAnalytics", body: ["purchaseSdkWebPageDidReportUALPageView:": "\(pageView)"])
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, webPageDidReportUALCommerceEvent commerceEvent: TicketmasterFoundation.UALCommerceEvent) {
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didBeginTicketSelectionFor event: TicketmasterDiscoveryAPI.DiscoveryEvent) {
+    //        sendEvent("igniteAnalytics", body: ["purchaseSdkDidBeginTicketSelectionFor:": "\(event)"])
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didEndTicketSelectionFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, because reason: TicketmasterPurchase.TMEndTicketSelectionReason) {
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didBeginCheckoutFor event: TicketmasterDiscoveryAPI.DiscoveryEvent) {
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didEndCheckoutFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, because reason: TicketmasterPurchase.TMEndCheckoutReason) {
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didMakePurchaseFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, order: TicketmasterPurchase.TMPurchaseOrder) {
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didPressNavBarButtonFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, button: TicketmasterPurchase.TMPurchaseNavBarButton) {
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didShare event: TicketmasterDiscoveryAPI.DiscoveryEvent, activityType: UIActivity.ActivityType) {
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didViewSubPageFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, subPage: TicketmasterPurchase.TMPurchaseSubPage) {
+    //        sendEvent("igniteAnalytics", body: ["purchaseSdkDidViewSubPageFor:": "\(event)"])
+    return
+  }
+  
+  func purchaseNavigationController(_ purchaseNavigationController: TicketmasterPurchase.TMPurchaseNavigationController, didMakeDecisionFor event: TicketmasterDiscoveryAPI.DiscoveryEvent, component: TicketmasterPurchase.TMPurchaseComponent, decision: TicketmasterPurchase.TMPurchaseDecision) {
+    return
+  }
 }
 
 
