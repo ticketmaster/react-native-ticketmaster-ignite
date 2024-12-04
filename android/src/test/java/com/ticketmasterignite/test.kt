0797ec35cb491c44f77b299af9189ec9424c0296
Kotauror <zygmunt.justyna@gmail.com>
2024-12-04 14:52:17 +0100

    test: setup kotlin tests

---
+++ android/src/test/java/com/ticketmasterignite/test.kt
@@ -0,0 +1,12 @@
+package com.ticketmasterignite
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class MyKotlinClassTest {
+
+    @Test
+    fun testExampleFunction() {
+        assertEquals(true, true)
+    }
+}
