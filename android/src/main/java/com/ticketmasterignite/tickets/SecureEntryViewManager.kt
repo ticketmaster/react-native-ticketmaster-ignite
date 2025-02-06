caaa33284739a3e0e845b757fa2a4c78b91d46ca
Daniel <daniel.olugbade@ticketmaster.co.uk>
2025-02-06 11:08:47 +0000

    chore: update imports

--- android/src/main/java/com/ticketmasterignite/tickets/SecureEntryViewManager.kt
+++ android/src/main/java/com/ticketmasterignite/tickets/SecureEntryViewManager.kt
@@ -8,7 +8,6 @@ import android.widget.FrameLayout
 import androidx.fragment.app.FragmentActivity
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableArray
-import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.ViewGroupManager
 import com.facebook.react.uimanager.annotations.ReactProp
