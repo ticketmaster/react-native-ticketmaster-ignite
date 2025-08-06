591f2197a506d16136b7ad21897116754d23986c
Daniel <daniel.olugbade@ticketmaster.co.uk>
2025-08-06 10:51:47 +0100

    refactor: unmountOnBlur tickets sdk

--- example/src/screens/MyEvents.tsx
+++ example/src/screens/MyEvents.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Platform, StyleSheet } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
 import { TicketsSdkEmbedded } from 'react-native-ticketmaster-ignite';
 
 const MyEvents = () => {
+  const isFocused = useIsFocused();
+
+  if (!isFocused) {
+    return null;
+  }
+
   return <TicketsSdkEmbedded style={styles.ticketsContainer} />;
 };
 
