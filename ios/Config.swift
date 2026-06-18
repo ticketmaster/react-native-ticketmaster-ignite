98f6382d9c90458a4ac9cab5835f7be91815855b
joe goodall <joe.goodall@ticketmaster.co.uk>
2026-06-18 16:50:39 +0100

    update Config.swift

--- ios/Config.swift
+++ ios/Config.swift
@@ -7,18 +7,18 @@ final class Config: NSObject {
   static let shared = Config()
   
   func get(for key: String) -> String {
-    return RCTNativeConfig.getConfig(forKey: key) ?? ""
+    return RCTNativeConfig.getForKey(key) ?? ""
   }
-  
+
   func set(for key: String, value: String) {
     RCTNativeConfig.setConfig(value, forKey: key)
   }
-  
+
   func optionalString(for key: String) -> String? {
-    return RCTNativeConfig.getConfig(forKey: key)
+    return RCTNativeConfig.getForKey(key)
   }
-  
+
   func getImage(for key: String) -> UIImage? {
-    return RCTNativeConfig.getImage(forKey: key)
+    return RCTNativeConfig.getImageForKey(key)
   }
 }
