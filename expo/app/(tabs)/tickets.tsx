e16ca4b373e5d1488824cd5e8819b2f6725c939e
Daniel <daniel.olugbade@ticketmaster.co.uk>
2026-07-16 11:25:22 +0100

    chore: bump sdk to 3.18.1

--- expo/app/(tabs)/tickets.tsx
+++ expo/app/(tabs)/tickets.tsx
@@ -1,6 +1,34 @@
-import React from 'react';
+import { useIsFocused } from 'expo-router';
+import { useContext, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 import { TicketsSdkEmbedded } from 'react-native-ticketmaster-ignite';
+import { AppContext, type AppContextType } from '@shared/contexts/AppContext';
 
 export default function TabThreeScreen() {
-  return <TicketsSdkEmbedded style={{ height: '100%', width: '100%' }} />;
+  const isFocused = useIsFocused();
+  const { ticketDeepLinkId, setTicketDeepLinkId } = useContext(
+    AppContext
+  ) as AppContextType;
+
+  useEffect(() => {
+    return () => {
+      setTicketDeepLinkId('');
+    };
+  }, [setTicketDeepLinkId]);
+
+  return (
+    <TicketsSdkEmbedded
+      style={styles.ticketsContainer}
+      offsetTop={0}
+      deepLinkId={isFocused ? ticketDeepLinkId : ''}
+      isFocused={isFocused}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  ticketsContainer: {
+    height: '100%',
+    width: '100%',
+  },
+});
